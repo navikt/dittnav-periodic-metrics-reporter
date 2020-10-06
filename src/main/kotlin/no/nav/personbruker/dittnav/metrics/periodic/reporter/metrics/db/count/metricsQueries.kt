337747cf5a0a7037930da07298ad71d335a79303
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-10-06 14:50:02 +0200

    Bytter til en raskere spørring for å telle antall eventer per produsent. Dropper å gå mot brukernotifikasjon-view-et.

--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/db/count/metricsQueries.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/db/count/metricsQueries.kt
@@ -31,11 +31,22 @@ fun Connection.countTotalNumberOfEventsByActiveStatus(eventType: EventType, akti
 
 fun Connection.countTotalNumberOfBrukernotifikasjonerByActiveStatus(aktiv: Boolean): Map<String, Int> {
     return prepareStatement(
-            "SELECT systembruker, COUNT(*) FROM brukernotifikasjon_view WHERE aktiv = ? GROUP BY systembruker;",
+"""SELECT
+                subquery.systembruker, sum(count)
+        FROM (
+             SELECT systembruker, COUNT(1) as count FROM BESKJED WHERE aktiv = ? GROUP BY systembruker
+             UNION ALL
+             SELECT systembruker, COUNT(1) as count FROM OPPGAVE WHERE aktiv = ? GROUP BY systembruker
+             UNION ALL
+             SELECT systembruker, COUNT(1) as count FROM INNBOKS WHERE aktiv = ? GROUP BY systembruker
+        ) as subquery group by subquery.systembruker order by subquery.systembruker;
+    """,
             ResultSet.TYPE_SCROLL_INSENSITIVE,
             ResultSet.CONCUR_READ_ONLY)
             .use { statement ->
                 statement.setBoolean(1, aktiv)
+                statement.setBoolean(2, aktiv)
+                statement.setBoolean(3, aktiv)
                 val resultSet = statement.executeQuery()
                 mutableMapOf<String, Int>().apply {
                     while (resultSet.next()) {
