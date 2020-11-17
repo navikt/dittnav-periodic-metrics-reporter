4ccdafd6678cc0f8faabdad16ebb232a351d41c8
Truls Jørgensen <trulsjor@gmail.com>
2020-11-17 09:30:40 +0100

    La til en EnvironmentTest.kt som benytter kotest for å manipulere env vars under test.

--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/config/Environment.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/config/Environment.kt
@@ -13,12 +13,12 @@ data class Environment(val bootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP
                        val clusterName: String = getEnvVar("NAIS_CLUSTER_NAME"),
                        val namespace: String = getEnvVar("NAIS_NAMESPACE"),
                        val sensuHost: String = getEnvVar("SENSU_HOST"),
-                       val sensuPort: String = getEnvVar("SENSU_PORT")
+                       val sensuPort: String = getEnvVar("SENSU_PORT"),
+                       val deltaCountingModeActive: Boolean = (System.getenv("DELTA_COUNTING_MODE")?:"false").toBoolean()
 )
 
 fun getEnvVar(varName: String): String {
     return System.getenv(varName)
             ?: throw IllegalArgumentException("Appen kan ikke starte uten av miljøvariabelen $varName er satt.")
 }
-
 fun isOtherEnvironmentThanProd() = System.getenv("NAIS_CLUSTER_NAME") != "prod-sbs"
