6021bec8ed6220fcac0b0062b98bfc8b5ad8e9c5
Christopher Santana <christopher.santana@nav.no>
2021-09-02 14:27:13 +0200

    Legg til telling av eventId-er per format.

--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/events/EventId.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/events/EventId.kt
@@ -1,23 +1,37 @@
 package no.nav.personbruker.dittnav.metrics.periodic.reporter.metrics.kafka.topic.events
 
-interface EventId
+interface EventId {
+    fun getFormat(): EventIdFormat
+}
 
 data class EventIdPlainText (
         val stringValue: String
-): EventId
+): EventId {
+    override fun getFormat() = EventIdFormat.PLAINTEXT
+}
 
 data class EventIdUuid (
         val lowBits: Long,
         val highBits: Long
-): EventId
+): EventId {
+    override fun getFormat() = EventIdFormat.UUID
+}
 
 data class EventIdPrefixedUuid (
         val prefix: Char,
         val lowBits: Long,
         val highBits: Long
-): EventId
+): EventId {
+    override fun getFormat() = EventIdFormat.PREFIXED_UUID
+}
 
 data class EventIdUlid (
         val lowBits: Long,
         val highBits: Long
-): EventId
+): EventId {
+    override fun getFormat() = EventIdFormat.ULID
+}
+
+enum class EventIdFormat {
+    PLAINTEXT, UUID, PREFIXED_UUID, ULID
+}
--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/events/PerProducerTracker.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/events/PerProducerTracker.kt
@@ -7,12 +7,40 @@ class PerProducerTracker(initialEntry: UniqueKafkaEventIdentifier) {
 
     private val userEventIds = HashSet<UserEventIdEntry>()
 
+    private val eventIdFormatCounter = EventIdFormatCounter()
+
+    fun countEventIdsByFormat(): Map<EventIdFormat, Int> = eventIdFormatCounter.formatCount
+
     fun addEvent(uniqueKafkaEventIdentifier: UniqueKafkaEventIdentifier): Boolean {
-        return userEventIds.add(UserEventIdEntry.fromUniqueIdentifier(uniqueKafkaEventIdentifier))
+        val entry = UserEventIdEntry.fromUniqueIdentifier(uniqueKafkaEventIdentifier)
+
+        return if (userEventIds.add(entry)) {
+            eventIdFormatCounter.registerEntry(entry)
+            true
+        } else {
+            false
+        }
     }
 
     init {
         userEventIds.add(UserEventIdEntry.fromUniqueIdentifier(initialEntry))
+    }
+}
+
+private class EventIdFormatCounter {
+
+    val formatCount = mutableMapOf<EventIdFormat, Int>()
+
+    fun registerEntry(entry: UserEventIdEntry) {
+        val format = entry.eventId.getFormat()
+
+        formatCount.compute(format) { _, counter ->
+            if (counter == null) {
+                1
+            } else {
+                counter + 1
+            }
+        }
     }
 }
 
