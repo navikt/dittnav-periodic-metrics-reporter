686d4116e5a491fa902ecc9016a991de6318db04
Robert Larsen <robert.larsen@nav.no>
2021-10-21 16:11:28 +0200

    Legger til telling av Innboks fra cache on-prem + Aiven-topics.

--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/TopicEventCounterAivenService.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/periodic/reporter/metrics/kafka/topic/TopicEventCounterAivenService.kt
@@ -29,6 +29,12 @@ class TopicEventCounterAivenService<K>(
                 async { TopicMetricsSession(EventType.OPPGAVE_INTERN) }
             }
 
+            val innboks = if(isOtherEnvironmentThanProd()) {
+                innboksCounter.countEventsAsync()
+            } else {
+                async { TopicMetricsSession(EventType.INNBOKS_INTERN) }
+            }
+
             val done = if(isOtherEnvironmentThanProd()) {
                 doneCounter.countEventsAsync()
             } else {
@@ -41,7 +47,6 @@ class TopicEventCounterAivenService<K>(
                 async { TopicMetricsSession(EventType.FEILRESPONS) }
             }
 
-            val innboks = async { TopicMetricsSession(EventType.INNBOKS_INTERN) }
             val statusoppdateringer = async { TopicMetricsSession(EventType.STATUSOPPDATERING_INTERN) }
 
             val sessions = CountingMetricsSessions()
